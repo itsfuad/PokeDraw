cff79e5fc8abf26304fda897d104b5aa5622b0d9
Fuad Hasan <fuad.cs22@gmail.com>
2023-08-06 21:00:58 +0600

    ..

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 console.log('app.js');
 
 const brushSizes = {
+    xsmall: 2.5,
     small: 5,
     medium: 10,
     large: 20
@@ -13,7 +14,6 @@ const brushModes = {
 
 let hoverBrushSize = 5 / 2;
 
-
 const colorPalatte = document.querySelector('.color-palatte');
 
 colorPalatte.addEventListener('click', (e) => {
@@ -24,7 +24,7 @@ colorPalatte.addEventListener('click', (e) => {
 
 const brushTypes = document.querySelector('.brushTypes');
 
-brushTypes.addEventListener('click', (e)=>{
+brushTypes.addEventListener('click', (e) => {
     const selectedBrush = brushTypes.querySelector('input:checked');
     console.log(selectedBrush.id);
     drawMode = selectedBrush.id;
@@ -46,7 +46,7 @@ let color = 'black';
 
 let eraseMode = false;
 
-let brushSize = 'small';
+let brushSize = 'xsmall';
 
 let canvasSnapshot = null;
 
@@ -58,274 +58,6 @@ let lastY = null;
 
 let isDrawing = false;
 
-/**
- * @type {HTMLCanvasElement}
- */
-const canvas = document.querySelector('.drawingSheet');
-/**
- * @type {CanvasRenderingContext2D}
- */
-const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-ctx.getContextAttributes().willReadFrequently = true;
-
-/**
- * @type {HTMLCanvasElement}
- */
-const brushPositionCanvas = document.querySelector('.brushPosition');
-/**
- * @type {CanvasRenderingContext2D}
-*/
-const brushPositionCtx = brushPositionCanvas.getContext('2d');
-brushPositionCanvas.width = window.innerWidth;
-brushPositionCanvas.height = window.innerHeight;
-brushPositionCtx.getContextAttributes().willReadFrequently = true;
-
-
-function drawBrush(e) {
-    //draw a hollow circle
-    brushPositionCtx.clearRect(0, 0, brushPositionCanvas.width, brushPositionCanvas.height);
-    brushPositionCtx.beginPath();
-    brushPositionCtx.arc(e.clientX, e.clientY, hoverBrushSize, 0, 2 * Math.PI);
-    brushPositionCtx.stroke();
-}
-
-/**
- * 
- * @param {PointerEvent} e
- */
-function handlePointerMove(e) {
-
-    drawBrush(e);
-    //console.log(e.clientX, e.clientY);
-    if (isDrawing) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        if (canvasSnapshot) {
-            ctx.putImageData(canvasSnapshot, 0, 0);
-        }
-        draw(e);
-    }
-}
-
-/**
- * {TouchEvent} e
- * @param {TouchEvent} e
- */
-
-function handleTouchMove(e) {
-    drawBrush(e.touches[0]);
-    if (isDrawing) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        if (canvasSnapshot) {
-            ctx.putImageData(canvasSnapshot, 0, 0);
-        }
-        draw(e.touches[0]);
-    }
-}
-
-let pressure = 1;
-
-/**
- * 
- * @param {PointerEvent} e 
- */
-function handlePointerDown(e) {
-    e.preventDefault();
-    isDrawing = true;
-
-    ctx.beginPath();
-    ctx.lineWidth = brushSizes[brushSize];
-    ctx.lineCap = "round";
-    ctx.lineJoin = "round";
-    ctx.strokeStyle = color;
-    ctx.lineWidth = brushSizes[brushSize] * pressure;
-    [lastX, lastY] = [e.clientX, e.clientY];
-    canvasSnapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
-}
-
-/**
- * 
- * @param {TouchEvent} e 
- */
-function handleTouchDown(e) {
-    isDrawing = true;
-    ctx.beginPath();
-    ctx.lineWidth = brushSizes[brushSize];
-    ctx.lineCap = "round";
-    ctx.lineJoin = "round";
-    ctx.strokeStyle = color;
-    ctx.lineWidth = brushSizes[brushSize];
-    [lastX, lastY] = [e.touches[0].clientX, e.touches[0].clientY];
-    canvasSnapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
-}
-
-function handlePointerUp() {
-    isDrawing = false;
-    endDraw();
-}
-
-function handleTouchUp() {
-    isDrawing = false;
-    endDraw();
-}
-
-
-/**
- * 
- * @param {PointerEvent | TouchEvent} e 
- * @returns 
- */
-function draw(e) {
-    if (!isDrawing) return;
-    if (drawMode === 'pen') {
-        drawPen(e);
-    } else if (drawMode === 'line') {
-        drawLine(e);
-    } else if (drawMode === 'rectangle') {
-        drawRectangle(e);
-    } else if (drawMode === 'circle') {
-        drawCircle(e);
-    } else if (drawMode === 'triangle') {
-        drawTriangle(e);
-    } else if (drawMode === 'eraser') {
-        //TODO
-        erase(e);
-    }
-}
-
-/**
- * 
- * @param {PointerEvent | TouchEvent} e 
- * @returns 
- */
-function erase(e) {
-    ctx.globalCompositeOperation = brushModes[drawMode];
-    //ctx.beginPath();
-    ctx.lineWidth = brushSizes[brushSize] * 10;
-    ctx.moveTo(lastX, lastY);
-    ctx.lineTo(e.clientX, e.clientY);
-    ctx.stroke();
-    [lastX, lastY] = [e.clientX, e.clientY]; // Update lastX and lastY for the next stroke
-}
-
-/**
- * 
- * @param {PointerEvent | TouchEvent} e 
- * @returns 
- */
-function drawPen(e) {
-    //ctx.beginPath(); // Start a new path for each pen stroke
-    ctx.globalCompositeOperation = brushModes[drawMode];
-    ctx.lineWidth = brushSizes[brushSize];
-    ctx.moveTo(lastX, lastY);
-    ctx.lineTo(e.clientX, e.clientY);
-    ctx.stroke();
-
-    [lastX, lastY] = [e.clientX, e.clientY]; // Update lastX and lastY for the next stroke
-}
-
-/**
- * 
- * @param {PointerEvent | TouchEvent} e 
- * @returns 
- */
-function drawLine(e) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    if (canvasSnapshot) {
-        ctx.putImageData(canvasSnapshot, 0, 0);
-    }
-    ctx.globalCompositeOperation = brushModes[drawMode];
-    ctx.beginPath();
-    ctx.moveTo(lastX, lastY);
-    ctx.lineTo(e.clientX, e.clientY);
-    ctx.stroke();
-}
-
-/**
- * 
- * @param {PointerEvent | TouchEvent} e 
- * @returns 
- */
-function drawRectangle(e) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    if (canvasSnapshot) {
-        ctx.putImageData(canvasSnapshot, 0, 0);
-    }
-    ctx.globalCompositeOperation = brushModes[drawMode];
-    ctx.beginPath();
-    ctx.rect(lastX, lastY, e.clientX - lastX, e.clientY - lastY);
-    ctx.stroke();
-}
-
-/**
- * 
- * @param {PointerEvent | TouchEvent} e 
- * @returns 
- */
-function drawCircle(e) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    if (canvasSnapshot) {
-        ctx.putImageData(canvasSnapshot, 0, 0);
-    }
-    ctx.globalCompositeOperation = brushModes[drawMode];
-    ctx.beginPath();
-    ctx.arc(lastX, lastY, Math.sqrt(Math.pow(e.clientX - lastX, 2) + Math.pow(e.clientY - lastY, 2)), 0, 2 * Math.PI);
-    ctx.stroke();
-}
-
-/**
- * 
- * @param {PointerEvent | TouchEvent} e 
- * @returns 
- */
-function drawTriangle(e) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    if (canvasSnapshot) {
-        ctx.putImageData(canvasSnapshot, 0, 0);
-    }
-    ctx.globalCompositeOperation = brushModes[drawMode];
-    ctx.beginPath();
-    ctx.moveTo(lastX, lastY);
-    ctx.lineTo(e.clientX, e.clientY);
-    ctx.lineTo(lastX, e.clientY);
-    ctx.closePath();
-    ctx.stroke();
-}
-
-
-function endDraw() {
-    if (lastX !== null && lastY !== null) { // Check if there was any drawing action
-        undoStack.push(canvas.toDataURL());
-        //console.log('Drawing ended');
-        redoStack = [];
-        isDrawing = false;
-        enableRedo = false;
-        if (undoStack.length > 20) {
-            undoStack.shift();
-        }
-        //console.log(undoStack.length);
-        if (undoStack.length > 0) {
-            undoButton.disabled = false;
-        }
-        redoButton.disabled = true;
-    }
-    lastX = null; // Reset lastX and lastY after ending the draw
-    lastY = null;
-}
-
-
-canvas.addEventListener('pointerdown', handlePointerDown);
-canvas.addEventListener('pointermove', handlePointerMove);
-canvas.addEventListener('pointerup', handlePointerUp);
-canvas.addEventListener('pointerleave', handlePointerUp);
-
-
-canvas.addEventListener('touchstart', handleTouchDown);
-canvas.addEventListener('touchmove', handleTouchMove);
-canvas.addEventListener('touchend', handleTouchUp);
-
-
 const undoButton = document.getElementById('undo');
 const redoButton = document.getElementById('redo');
 const clearButton = document.getElementById('clear');
@@ -334,57 +66,326 @@ undoButton.addEventListener('click', handleUndo);
 redoButton.addEventListener('click', handleRedo);
 clearButton.addEventListener('click', handleClear);
 
-undoStack.push(canvas.toDataURL()); // Save the initial state of the canvas
+/**
+ * @type {HTMLCanvasElement}
+ */
+const canvas = document.querySelector('.drawingSheet');
+/**
+ * @type {CanvasRenderingContext2D}
+ */
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth * 0.80;
+canvas.height = window.innerHeight * 0.75;
+console.log(canvas.width, canvas.height);
+ctx.getContextAttributes().willReadFrequently = true;
 
-function handleClear() {
-    const ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    undoStack.length = 0;
-    redoStack.length = 0;
-    undoStack.push(canvas.toDataURL());
-    undoButton.disabled = true;
-    redoButton.disabled = true;
+/**
+ * @type {HTMLCanvasElement}
+ */
+const brushPositionCanvas = document.querySelector('.brushPosition');
+/**
+ * @type {CanvasRenderingContext2D}
+ */
+const brushPositionCtx = brushPositionCanvas.getContext('2d');
+brushPositionCanvas.width = window.innerWidth * 0.80;
+brushPositionCanvas.height = window.innerHeight * 0.75;
+brushPositionCtx.getContextAttributes().willReadFrequently = true;
+
+/**
+ * 
+ * @param {PointerEvent | TouchEvent} e
+ */
+function draw(e) {
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.clientX, e.clientY);
+    ctx.strokeStyle = color;
+    ctx.globalCompositeOperation = brushModes[drawMode];
+    ctx.lineWidth = brushSizes[brushSize] * (drawMode == 'eraser' ? 10 : 1);
+    ctx.lineCap = 'round';
+    ctx.stroke();
+
+    //console.log('draw', e.clientX, e.clientY, isDrawing, undoStack.length, redoStack.length);
+
+    lastX = e.clientX;
+    lastY = e.clientY;
 }
 
-function handleUndo() {
-    if (undoStack.length > 1) { // Check if there's at least one state in the undo stack
-        redoStack.push(undoStack.pop()); // Move the current state to the redo stack
-        const undoDataURL = undoStack[undoStack.length - 1]; // Get the last state from the undo stack
-        const image = new Image();
-        image.src = undoDataURL;
-        image.onload = () => {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.drawImage(image, 0, 0); // Draw the image on the canvas
-        };
+// draw straight line
+function drawLine(e) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        // Enable/disable undo and redo buttons
-        undoButton.disabled = undoStack.length === 1;
-        redoButton.disabled = false; // Redo button should be enabled if there's a state in the redo stack
+    if (undoStack.length > 0) {
+        ctx.putImageData(undoStack[undoStack.length - 1], 0, 0);
+    }
+  
+    let endX = e.clientX;
+    let endY = e.clientY;
+  
+    if (isShiftKeyDown) {
+      const deltaX = Math.abs(endX - lastX);
+      const deltaY = Math.abs(endY - lastY);
+      if (deltaX > deltaY) {
+        // Draw a purely horizontal line
+        endY = lastY;
+      } else {
+        // Draw a purely vertical line
+        endX = lastX;
+      }
+    }
+  
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(endX, endY);
+    ctx.strokeStyle = color;
+    ctx.globalCompositeOperation = brushModes[drawMode];
+    ctx.lineWidth = brushSizes[brushSize] * (drawMode == 'eraser' ? 10 : 1);
+    ctx.lineCap = 'round';
+    ctx.stroke();
+}
+
+//draw rectangle
+function drawRectangle(e) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (undoStack.length > 0) {
+        ctx.putImageData(undoStack[undoStack.length - 1], 0, 0);
+    }
+
+    let endX = e.clientX;
+    let endY = e.clientY;
+
+    
+    if (isShiftKeyDown){
+        //draw square
+        ctx.beginPath();
+        ctx.rect(lastX, lastY, Math.abs(endX - lastX), Math.abs(endX - lastX));
+        ctx.strokeStyle = color;
+        ctx.globalCompositeOperation = brushModes[drawMode];
+        ctx.lineWidth = brushSizes[brushSize] * (drawMode == 'eraser' ? 10 : 1);
+        ctx.lineCap = 'round';
+        ctx.stroke();
+    } else {
+        //draw rectangle
+        ctx.beginPath();
+        ctx.rect(lastX, lastY, Math.abs(endX - lastX), Math.abs(endY - lastY));
+        ctx.strokeStyle = color;
+        ctx.globalCompositeOperation = brushModes[drawMode];
+        ctx.lineWidth = brushSizes[brushSize] * (drawMode == 'eraser' ? 10 : 1);
+        ctx.lineCap = 'round';
+        ctx.stroke();
     }
 }
 
+//draw a ellipse
+function drawEllipse(e){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (undoStack.length > 0) {
+        ctx.putImageData(undoStack[undoStack.length - 1], 0, 0);
+    }
+
+    let endX = e.clientX;
+    let endY = e.clientY;
+
+    if (isShiftKeyDown){
+        //draw a circle
+        ctx.beginPath();
+        ctx.arc(lastX, lastY, Math.abs(endX - lastX), 0, 2 * Math.PI);
+        ctx.strokeStyle = color;
+        ctx.globalCompositeOperation = brushModes[drawMode];
+        ctx.lineWidth = brushSizes[brushSize] * (drawMode == 'eraser' ? 10 : 1);
+        ctx.lineCap = 'round';
+        ctx.stroke();
+    } else {
+        //draw a ellipse
+        ctx.beginPath();
+        ctx.ellipse(lastX, lastY, Math.abs(endX - lastX), Math.abs(endY - lastY), 0, 0, 2 * Math.PI);
+        ctx.strokeStyle = color;
+        ctx.globalCompositeOperation = brushModes[drawMode];
+        ctx.lineWidth = brushSizes[brushSize] * (drawMode == 'eraser' ? 10 : 1);
+        ctx.lineCap = 'round';
+        ctx.stroke();
+    }
+}
+
+//draw a triangle
+function drawTriangle(e){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (undoStack.length > 0) {
+        ctx.putImageData(undoStack[undoStack.length - 1], 0, 0);
+    }
+
+    let endX = e.clientX;
+    let endY = e.clientY;
+
+    if (isShiftKeyDown){
+        //draw a equilateral triangle
+        ctx.beginPath();
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(endX, lastY);
+        ctx.lineTo((lastX + endX) / 2, lastY - Math.abs(endX - lastX) * Math.sqrt(3) / 2);
+        ctx.closePath();
+        ctx.strokeStyle = color;
+        ctx.globalCompositeOperation = brushModes[drawMode];
+        ctx.lineWidth = brushSizes[brushSize] * (drawMode == 'eraser' ? 10 : 1);
+        ctx.lineCap = 'round';
+        ctx.stroke();
+    } else {
+        //draw a triangle
+        ctx.beginPath();
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(endX, endY);
+        ctx.lineTo(lastX, endY);
+        ctx.closePath();
+        ctx.strokeStyle = color;
+        ctx.globalCompositeOperation = brushModes[drawMode];
+        ctx.lineWidth = brushSizes[brushSize] * (drawMode == 'eraser' ? 10 : 1);
+        ctx.lineCap = 'round';
+        ctx.stroke();
+    }
+}
+
+// Function to handle mouse down event
+function handlePointerDown(e) {
+    isDrawing = true;
+    lastX = e.clientX;
+    lastY = e.clientY;
+
+    draw(e);
+}
+
+function drawBrushPosition(e){
+    brushPositionCtx.clearRect(0, 0, brushPositionCanvas.width, brushPositionCanvas.height);
+    brushPositionCtx.beginPath();
+    brushPositionCtx.arc(e.clientX, e.clientY, hoverBrushSize, 0, Math.PI * 2);
+    brushPositionCtx.strokeStyle = 'black';
+    brushPositionCtx.stroke();
+}
+
+function endDrawing(){
+    if (!isDrawing) {
+        return;
+    }
+    console.log('Drawing ended');
+    isDrawing = false;
+    lastX = null;
+    lastY = null;
+    canvasSnapshot = ctx.getImageData(0, 0, canvas.width, canvas.height)
+    undoStack.push(canvasSnapshot);
+    redoStack = [];
+    updateUndoRedoButtons();
+}
+
+function handlePointerMove(e) {
+    if (isDrawing) {
+        if (drawMode == 'pen' || drawMode == 'eraser'){
+            draw(e);
+        } else if (drawMode == 'line'){
+            drawLine(e);
+        } else if (drawMode == 'rectangle'){
+            drawRectangle(e);
+        } else if (drawMode == 'circle'){
+            drawEllipse(e);
+        } else if (drawMode == 'triangle'){
+            drawTriangle(e);
+        }
+    }
+    drawBrushPosition(e);
+}
+
+// Event Listeners
+brushPositionCanvas.addEventListener('mousemove', (e) => {
+    handlePointerMove(e);
+});
+
+
+
+brushPositionCanvas.addEventListener('mousedown', handlePointerDown);
+brushPositionCanvas.addEventListener('mouseup', endDrawing);
+brushPositionCanvas.addEventListener('mouseleave', endDrawing);
+
+document.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+
+
+// Add a flag to track if the Shift key is currently held down
+let isShiftKeyDown = false;
+
+// Function to handle keydown event
+document.addEventListener('keydown', (e) => {
+  if (e.shiftKey) {
+    isShiftKeyDown = true;
+  }
+  if (e.key === 'z' && e.ctrlKey) {
+    handleUndo();
+  } else if (e.key === 'y' && e.ctrlKey) {
+    handleRedo();
+  } else if (e.key === 'c' && e.ctrlKey) {
+    handleClear();
+  } else if (e.key === 's' && e.ctrlKey) {
+    e.preventDefault();
+    handleSave();
+  }
+});
+
+// Function to handle keyup event
+document.addEventListener('keyup', (e) => {
+  if (!e.shiftKey) {
+    isShiftKeyDown = false;
+  }
+});
+
+// Function to undo the last action
+function handleUndo() {
+    if (undoStack.length > 0) {
+        redoStack.push(undoStack.pop());
+        if (undoStack.length > 0) {
+            ctx.putImageData(undoStack[undoStack.length - 1], 0, 0);
+        } else {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+        }
+        updateUndoRedoButtons();
+    }
+}
+
+// Function to redo the last undone action
 function handleRedo() {
     if (redoStack.length > 0) {
-        undoStack.push(redoStack.pop()); // Move the current state to the undo stack
-        const redoDataURL = undoStack[undoStack.length - 1]; // Get the last state from the undo stack
-        const image = new Image();
-        image.src = redoDataURL;
-        image.onload = () => {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.drawImage(image, 0, 0); // Draw the image on the canvas
-        };
-
-        // Enable/disable undo and redo buttons
-        undoButton.disabled = false; // Undo button should be enabled if there's a state in the undo stack
-        redoButton.disabled = redoStack.length === 0;
+        undoStack.push(redoStack.pop());
+        ctx.putImageData(undoStack[undoStack.length - 1], 0, 0);
+        updateUndoRedoButtons();
     }
 }
 
+// Function to clear the canvas
+function handleClear() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    undoStack = [];
+    redoStack = [];
+    updateUndoRedoButtons();
+}
 
-//save 
-document.getElementById('saveButton').addEventListener('click', ()=>{
-    const link = document.createElement("a");
-    link.download = `canvas-${Date.now()}.png`;
-    link.href = canvas.toDataURL();
+// Function to update undo and redo button states
+function updateUndoRedoButtons() {
+    undoButton.disabled = undoStack.length === 0;
+    redoButton.disabled = redoStack.length === 0;
+}
+
+// Initialize the undo and redo buttons
+updateUndoRedoButtons();
+
+const saveButton = document.getElementById('saveButton');
+saveButton.addEventListener('click', handleSave);
+
+function handleSave() {
+    const dataURL = canvas.toDataURL();
+    const link = document.createElement('a');
+    const fileName = `pokedraw-${Date.now()}.png`;
+    link.download = fileName;
+    link.href = dataURL;
     link.click();
-});
+}
